bfe4b1250655217b1ddbf95be8d449369ff9d5b1
BlaiseCz <blazej.czekala@gmail.com>
2021-03-01 13:47:28 +0100

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+    #zaraz po wczytaniu robimy macierz odleglosci, a nie wspolrzedne na plaszczyznie
+
+    #zaimplementowac wczytywanie instancji kroa100 krob100 i obliczyc macierz odleglosci
+
+    #algorytm zachlanny - najblizszy sasiad
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
