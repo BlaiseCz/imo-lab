f3392a212d766aa9d60201c554c6edec7d3f6843
BlaiseCz <blazej.czekala@gmail.com>
2021-03-02 20:45:50 +0100

    greedy cycle added

--- greedy_cycle.py
+++ greedy_cycle.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 
+
 def greedy_cycle(distance_matrix, start_with):
     cycle = [start_with]
     history = [cycle]
@@ -12,27 +13,45 @@ def greedy_cycle(distance_matrix, start_with):
     history.append(deepcopy(cycle))
 
     while not len(cycle) == len(distance_matrix):
-        nearest_node_id = find_node_closest_to_current_cycle(cycle, distance_matrix)
-        cycle.append(nearest_node_id)
+        cycle = find_node_closest_to_current_cycle(cycle, distance_matrix)
         history.append(deepcopy(cycle))
 
+    print(f'dist: {calculate_dist_of_given_cycle(cycle, distance_matrix)}')
     return cycle, history
 
 
 def find_node_closest_to_current_cycle(current_cycle, distance_matrix):
-    min_node_id = len(distance_matrix)
-    min_dist = {}
+    nodes_calculated_cycles = []
 
-    for cycle_node_id in current_cycle:
-        node = distance_matrix[cycle_node_id]
-        min_node_dist = max(node)
+    for node_id in range(len(distance_matrix)):
+        if node_id not in current_cycle:
+            nodes_calculated_cycles.append(calculate_dist_with_new_node(current_cycle, node_id, distance_matrix))
 
-        for node_id, node_dist in enumerate(node):
-            if node_id not in current_cycle:
-                if node_dist < min_node_dist:
-                    min_node_dist = node_dist
-                    min_node_id = node_id
-        min_dist[min_node_id] = min_node_dist
+    best_match_cycle = min(nodes_calculated_cycles, key=lambda x: x['path_dist'])
+    return best_match_cycle['cycle']
 
-    min_node_id = min(min_dist, key=min_dist.get)
-    return min_node_id
+
+def calculate_dist_with_new_node(cycle, node_to_add, distance_matrix):
+    best_path_cycle_dict = []
+
+    for x in range(len(cycle) + 1):
+        updated_cycle = deepcopy(cycle)
+        updated_cycle.insert(x, node_to_add)
+
+        best_path_cycle_dict.append(
+            {
+                'node_to_dad': node_to_add,
+                'path_dist': calculate_dist_of_given_cycle(updated_cycle, distance_matrix),
+                'cycle': updated_cycle
+            })
+
+    return min(best_path_cycle_dict, key=lambda x: x['path_dist'])
+
+
+def calculate_dist_of_given_cycle(cycle, distance_matrix):
+    dist = 0
+
+    for x in range(len(cycle) - 1):
+        dist += distance_matrix[cycle[x]][cycle[x + 1]]
+
+    return dist
