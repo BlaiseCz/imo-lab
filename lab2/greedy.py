8c0c340e41caece51a4018a55db3653eb3c36372
BlaiseCz <blazej.czekala@gmail.com>
2021-03-21 14:20:47 +0100

    greedy_nodes

--- lab2/greedy.py
+++ lab2/greedy.py
@@ -31,3 +31,30 @@ def greedy_edges(path, distance_matrix, swap=2):
             better_solution = False
 
     return best_path, best_dist, history
+
+
+def greedy_nodes(path, distance_matrix, swap=2):
+    better_solution = True
+    better_solution_found = False
+    best_path = deepcopy(path)
+    buff_path = deepcopy(path)
+    best_dist = calculate_distance(distance_matrix, path)
+
+    history = [deepcopy(path)]
+
+    while better_solution:
+        node_to_switch = sample(list(range(len(distance_matrix))), 2)
+
+        buff_path[node_to_switch[0]], buff_path[node_to_switch[1]] = buff_path[node_to_switch[1]], buff_path[node_to_switch[0]]
+        updated_dist = calculate_distance(distance_matrix, buff_path)
+
+        if updated_dist < best_dist:
+            best_dist = updated_dist
+            best_path = buff_path
+            print(f'best dist = {best_dist}')
+            better_solution_found = True
+            history.append(deepcopy(best_path))
+        if not better_solution_found:
+            better_solution = False
+
+    return best_path, best_dist, history
