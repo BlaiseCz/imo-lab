e3fc6e0cabeb1688dcae7ef2e9979bfef0e977c6
Marek Subocz <mrksubocz@gmail.com>
2021-04-17 00:49:36 +0200

    dodana prawidłowa metoda proponowania zamiany wierzchołków

--- lab3/propose.py
+++ lab3/propose.py
@@ -5,13 +5,52 @@ from copy import deepcopy
 from lab1.distance_counter import calculate_distance
 from lab2.local_search import set_ids_order
 
+def propose_nodes_switch(distance_matrix, cycle1: list, cycle2: list):
+    for node1, node2 in itertools.combinations(cycle1 + cycle2, 2):
+        # sprawdzamy w których cyklach są wybrane node'y
+        where = []
+        if node1 in cycle1:
+            i1 = cycle1.index(node1)
+            where.append(0)
+        else:
+            i1 = cycle2.index(node1)
+            where.append(1)
+
+        if node2 in cycle1:
+            i2 = cycle1.index(node2)
+            where.append(0)
+        else:
+            i2 = cycle2.index(node2)
+            where.append(1)
+
+        # liczymy gaina z zamiany
+        # wierzchołki są w różnych cyklach
+        if where == [1,0]:
+            # chcemy mieć najpierw cykl1, potem cykl2
+            node1, node2 = node2, node1
+            i1, i2 = i2, i1
+            where = [0,1]
+            num = 2
+            gain = gain_switch_nodes_between(distance_matrix, cycle1, cycle2, i1,i2)
+        # oba wierzchołki są w tym samym cyklu
+        else:
+            if where[0] == 0:
+                gain = gain_switch_nodes(cycle1, distance_matrix, i1, i2)
+                num = 0
+            else:
+                gain = gain_switch_nodes(cycle2, distance_matrix, i1, i2)
+                num = 1
+
+        yield gain, i1, i2, num
+
+
 
 def propose_in_route(distance_matrix, cycle1, cycle2):
     # first cycle
     combi1 = list(itertools.combinations(range(len(cycle1)), 2))
     random.shuffle(combi1)
     for i1, i2 in combi1:
-        gain1 = switch_nodes(cycle1, distance_matrix, i1, i2)
+        gain1 = gain_switch_nodes(cycle1, distance_matrix, i1, i2)
         gain2 = switch_edges(cycle1, distance_matrix, i1, i2)
 
         if gain1 > gain2:
@@ -27,7 +66,7 @@ def propose_in_route(distance_matrix, cycle1, cycle2):
     combi2 = list(itertools.combinations(range(len(cycle2)), 2))
     random.shuffle(combi2)
     for i1, i2 in combi2:
-        gain1 = switch_nodes(cycle2, distance_matrix, i1, i2)
+        gain1 = gain_switch_nodes(cycle2, distance_matrix, i1, i2)
         gain2 = switch_edges(cycle2, distance_matrix, i1, i2)
 
         if gain1 > gain2:
@@ -40,7 +79,7 @@ def propose_in_route(distance_matrix, cycle1, cycle2):
         yield gain, i1, i2, num
 
 
-def switch_nodes(cycle, distance_matrix, i1, i2):
+def gain_switch_nodes(cycle, distance_matrix, i1, i2):
     gain = 0
     if (i1 - 1) % len(cycle) != i2:
         gain += distance_matrix[cycle[i1]][cycle[(i1 - 1) % len(cycle)]] + \
@@ -54,6 +93,18 @@ def switch_nodes(cycle, distance_matrix, i1, i2):
                 distance_matrix[cycle[i2]][cycle[(i1 + 1) % len(cycle)]]
     return gain
 
+def gain_switch_nodes_between(distance_matrix, cycle1, cycle2, i1, i2):
+    gain = 0
+    gain += distance_matrix[cycle1[i1]][cycle1[(i1 - 1) % len(cycle1)]] + \
+            distance_matrix[cycle1[i1]][cycle1[(i1 + 1) % len(cycle1)]] + \
+            distance_matrix[cycle2[i2]][cycle2[(i2 - 1) % len(cycle2)]] + \
+            distance_matrix[cycle2[i2]][cycle2[(i2 + 1) % len(cycle2)]]
+
+    gain -= distance_matrix[cycle1[i1]][cycle2[(i2 - 1) % len(cycle2)]] + \
+            distance_matrix[cycle1[i1]][cycle2[(i2 + 1) % len(cycle2)]] + \
+            distance_matrix[cycle2[i2]][cycle1[(i1 - 1) % len(cycle1)]] + \
+            distance_matrix[cycle2[i2]][cycle1[(i1 + 1) % len(cycle1)]]
+    return gain
 
 def switch_edges(cycle, distance_matrix, i1, i2):
     first_node, second_node = set_ids_order(i1, i2)
@@ -86,3 +137,4 @@ def propose_between_routes(distance_matrix, cycle1, cycle2):
                 distance_matrix[cycle2[i2]][cycle1[(i1 + 1) % len(cycle1)]]
         # returning cycle1 index and cycle2 index to swap
         yield gain, i1, i2, 2
+
