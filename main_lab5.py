c677aef0db9841a6937a533f804ffac1a7246388
Marek Subocz <mrksubocz@gmail.com>
2021-05-28 20:44:45 +0200

    parents with results

--- main_lab5.py
+++ main_lab5.py
@@ -1,9 +1,8 @@
-from hall_of_fame.distance import calculate_distance
+from hall_of_fame.distance import calculate_distance, create_distance_matrix
 from hall_of_fame.local_search import steepest
 from hall_of_fame.read_file import read_file
 from hall_of_fame.start_result import k_regret
 from hall_of_fame.visualize import animate
-from lab1.distance_counter import count_dist
 import random
 
 
@@ -28,9 +27,11 @@ def recombine(sol1, sol2):
 if __name__ == '__main__':
     data_set = 'kroB'
     overview, coordinates = read_file('data/' + data_set + '100.tsp')
-    distance_matrix = count_dist(coordinates)
+    distance_matrix = create_distance_matrix(coordinates)
 
     parents = generate_solutions(distance_matrix)
+    parents = [(calculate_distance(distance_matrix, p), p) for p in parents]
+    parents = sorted(parents)
 
     counter = 0
 
