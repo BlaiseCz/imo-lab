76adddab9650c1d691ef989d175d2db1f66d09f6
Marek Subocz <mrksubocz@gmail.com>
2021-06-11 13:54:33 +0200

    wypuklosc dziala

--- wypuklosc.py
+++ wypuklosc.py
@@ -9,15 +9,13 @@ def generate_solutions(distance_matrix, num_of_sol=1000):
     for i in range(num_of_sol):
         print(f'{i+1}/{num_of_sol}')
         solution = random_path(distance_matrix)[-1]
-        print('got random')
         solution = steepest(distance_matrix, solution[0], solution[1])[-1]
-        print('got steepest')
         solutions.append(solution)
     return solutions
 
 def calculate_distances(distance_matrix, solutions):
     res = deepcopy(solutions)
-    for i in range(len(distance_matrix)):
+    for i in range(len(res)):
         dist = calculate_distance(distance_matrix, res[i])
         res[i] = (dist, res[i])
     return res
@@ -26,9 +24,10 @@ def calculate_similarities(solutions):
     res = deepcopy(solutions)
     res = sorted(res)
 
-    best = res[0]
+    best = res[0][1]
     for i, sol in enumerate(res[1:]):
-        res[i] = (*sol, calculate_similarity(best, sol[1]))
+        res[i+1] = (*sol, calculate_similarity(best, sol[1]))
+    res[0] = [*res[0], 0]
     return res
 
 def calculate_similarity(sol1, sol2):
@@ -51,15 +50,25 @@ def calculate_similarity(sol1, sol2):
 if __name__ == '__main__':
 
     data_set = 'kroB'
-    overview, coordinates = read_file('data/' + data_set + '200.tsp')
+    overview, coordinates = read_file('data/' + data_set + '100.tsp')
     distance_matrix = create_distance_matrix(coordinates)
 
-    solutions = generate_solutions(distance_matrix, 10)
+    # ustaw na ile rozwiązań to chcesz
+    num_of_solutions = 2
+
+    solutions = generate_solutions(distance_matrix, num_of_solutions)
     solutions = calculate_distances(distance_matrix, solutions)
     solutions = calculate_similarities(solutions)
 
     xs = [s[0] for s in solutions]
     ys = [s[2] for s in solutions]
     import matplotlib.pyplot as plt
-    plt.scatter(xs, ys)
+    plt.scatter(xs[0], ys[0], label='best solution')
+    plt.scatter(xs[1:], ys[1:], label='rest of solutions')
+    plt.legend()
+    plt.ylabel('Number of differences')
+    plt.xlabel('Result')
+    plt.title(f'Distribution of {num_of_solutions} solutions aquired by steepest search from random\n')
+    plt.savefig('wypuklosc.png')
+    plt.show()
 
