4823d3ef0ed65fa66f94fe448e614e7e3ea641bc
Marek Subocz <mrksubocz@gmail.com>
2021-06-14 15:05:40 +0200

    farewell

--- wypuklosc.py
+++ wypuklosc.py
@@ -2,14 +2,14 @@ from copy import deepcopy
 from hall_of_fame.read_file import read_file
 from hall_of_fame.distance import create_distance_matrix, calculate_distance
 from hall_of_fame.start_result import random_path
-from hall_of_fame.local_search import steepest
+from hall_of_fame.local_search import steepest, greedy
 
 def generate_solutions(distance_matrix, num_of_sol=1000):
     solutions = []
     for i in range(num_of_sol):
         print(f'{i+1}/{num_of_sol}')
         solution = random_path(distance_matrix)[-1]
-        solution = steepest(distance_matrix, solution[0], solution[1])[-1]
+        solution = greedy(distance_matrix, solution[0], solution[1])[-1]
         solutions.append(solution)
     return solutions
 
@@ -20,17 +20,22 @@ def calculate_distances(distance_matrix, solutions):
         res[i] = (dist, res[i])
     return res
 
-def calculate_similarities(solutions):
+def calculate_similarities(solutions, by_nodes=False):
     res = deepcopy(solutions)
     res = sorted(res)
 
     best = res[0][1]
     for i, sol in enumerate(res[1:]):
-        res[i+1] = (*sol, calculate_similarity(best, sol[1]))
+        res[i+1] = (*sol, calculate_similarity(best, sol[1], by_nodes))
     res[0] = [*res[0], 0]
     return res
 
-def calculate_similarity(sol1, sol2):
+def calculate_similarity(sol1, sol2, by_nodes):
+    if by_nodes:
+        res = 0
+        res += min(len(set(sol1[0])&set(sol2[0])), len(set(sol1[0])&set(sol2[1])))
+        res += min(len(set(sol1[1])&set(sol2[0])), len(set(sol1[1])&set(sol2[1])))
+        return res
     same_edges_counter = 0
     for cycle in sol1:
         for i, node in enumerate(cycle):
@@ -54,14 +59,20 @@ if __name__ == '__main__':
     distance_matrix = create_distance_matrix(coordinates)
 
     # ustaw na ile rozwiązań to chcesz
-    num_of_solutions = 2
+    num_of_solutions = 1000
 
     solutions = generate_solutions(distance_matrix, num_of_solutions)
-    solutions = calculate_distances(distance_matrix, solutions)
-    solutions = calculate_similarities(solutions)
+    solutions_with_dist = calculate_distances(distance_matrix, solutions)
+    solutions = calculate_similarities(solutions_with_dist)
+    solutions_by_nodes = calculate_similarities(solutions_with_dist, by_nodes=True)
 
     xs = [s[0] for s in solutions]
     ys = [s[2] for s in solutions]
+    from statistics import mean, stdev
+    from scipy.stats import pearsonr
+    print(f'meanxs: {mean(xs)} , stdevxs: {stdev(xs)}')
+    print(f'meanys: {mean(ys)} , stdevys: {stdev(ys)}')
+    print(f'correlation: {pearsonr(xs, ys)[0]}')
     import matplotlib.pyplot as plt
     plt.scatter(xs[0], ys[0], label='best solution')
     plt.scatter(xs[1:], ys[1:], label='rest of solutions')
@@ -71,4 +82,22 @@ if __name__ == '__main__':
     plt.title(f'Distribution of {num_of_solutions} solutions aquired by steepest search from random\n')
     plt.savefig('wypuklosc.png')
     plt.show()
+    plt.close()
 
+    print('*'*40)
+    print('By nodes:')
+
+xs = [s[0] for s in solutions_by_nodes]
+ys = [s[2] for s in solutions_by_nodes]
+print(f'meanxs: {mean(xs)} , stdevxs: {stdev(xs)}')
+print(f'meanys: {mean(ys)} , stdevys: {stdev(ys)}')
+print(f'correlation: {pearsonr(xs, ys)[0]}')
+import matplotlib.pyplot as plt
+plt.scatter(xs[0], ys[0], label='best solution')
+plt.scatter(xs[1:], ys[1:], label='rest of solutions')
+plt.legend()
+plt.ylabel('Number of differences')
+plt.xlabel('Result')
+plt.title(f'Distribution of {num_of_solutions} solutions aquired by steepest search from random\n')
+plt.savefig('wypuklosc_by_nodes_2.png')
+    # plt.show()
